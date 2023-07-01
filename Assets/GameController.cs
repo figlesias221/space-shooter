932d7a7948c56c854da89775d328096ed1feecc2
facundoalles <fcn2096@gmail.com>
2023-07-01 19:55:55 -0300

    Addd player name to highscore

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -6,9 +6,6 @@ public class GameController : MonoBehaviour
 {
     public GamePauseMenu pauseMenu;
     public GameObject gameOverMenu;
-    [SerializeField] ScoreManager scoreManager;
-    [SerializeField] HighScoreHandler highScoreHandler;
-    [SerializeField] string playerName;
     private bool gameOver;
 
     void Start()
@@ -31,16 +28,6 @@ public class GameController : MonoBehaviour
         {
             if (GameObject.FindWithTag("Player") == null)
             {
-                if (PlayerName.Name == "" || PlayerName.Name == null)
-                {
-                    playerName = "player1";
-                }
-                else
-                {
-                    playerName = PlayerName.Name;
-                }
-
-                highScoreHandler.AddHighScoreIfPossible(new HighScoreElement(playerName, scoreManager.Points));
                 gameOverMenu.SetActive(true);
                 Time.timeScale = 0;
                 gameOver = true;
--- Assets/GameOverMenu.cs
+++ Assets/GameOverMenu.cs
@@ -8,17 +8,36 @@ public class GameOverMenu : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI killsText;
+    public string playerName;
+    [SerializeField] HighScoreHandler highScoreHandler;
 
     public void Start()
     {
         string playerScore = PlayerPrefs.GetFloat("Player Score").ToString();
         string playerKills = PlayerPrefs.GetInt("Kills").ToString();
         scoreText.text = "Score: " + playerScore;
-        killsText.text = "Kills: " + playerKills;
-
+        killsText.text = "Kills: " + playerKills; 
     }
+
     public void OnMainMenu()
     {
+        if (PlayerName.Name == "" || PlayerName.Name == null)
+        {
+            playerName = "player1";
+        }
+        else
+        {
+            playerName = PlayerName.Name;
+        }
+
+        highScoreHandler.AddHighScoreIfPossible(new HighScoreElement(playerName, Mathf.RoundToInt(PlayerPrefs.GetFloat("Player Score"))));
+
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ReadInput(string input)
+    {
+        playerName = input;
+        PlayerName.Name = playerName; 
     }
 }
